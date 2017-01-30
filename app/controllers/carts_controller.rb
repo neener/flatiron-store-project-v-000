7182dacfa04202cdc168b1349f324befcff68beb
neener <janinemrosen@gmail.com>
2017-01-30 13:55:40 -0500

    Done.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,5 +4,12 @@ class CartsController < ApplicationController
 	end
 
 	def checkout
+		current_user.current_cart.line_items.each do |item|
+	      @item = Item.find(item.item_id)
+	      @item.inventory -= item.quantity
+	      @item.save
+	    end
+		redirect_to cart_path(current_user)
 	end
+
 end
