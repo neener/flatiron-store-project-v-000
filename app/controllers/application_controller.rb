a983b8790fa9d3e650f086dbae5b8ee956acf10f
neener <janinemrosen@gmail.com>
2017-01-30 11:36:13 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -6,7 +8,17 @@ class ApplicationController < ActionController::Base
   helper_method :current_cart
 
   def current_cart
-  	@current_cart ||= Cart.find(session[:cart_id]) if session[:cart_id]
+  	# binding.pry
+  	if session[:cart_id]
+ 		Cart.find(session[:cart_id])
+ 	else
+	 	cart = Cart.create
+	 	cart.user = current_user
+	 	cart.save
+	 	cart
+ 	end
   end
 
 end
+
+
